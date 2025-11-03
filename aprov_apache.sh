9c46c6685b40c0e179b74b7a63a2ba553e189ac1
dfmanosalvan22 <dfmanosalvan22@iesalbarregas.es>
2025-11-03 22:21:17 +0100

    Update aprov_apache.sh

--- aprov_apache.sh
+++ aprov_apache.sh
@@ -7,10 +7,6 @@ sudo apt update -y
 echo "Instalando apache, php, mysql, git y mariadb-client"
 sudo apt install -y apache2 php libapache2-mod-php php-mysql git mariadb-client
 
-echo "Habilitando modulo rewrite de apache"
-a2enmod rewrite
-systemctl restart apache2
-
 echo "Clonando aplicacion desde github"
 cd /tmp
 git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
@@ -26,7 +22,6 @@ sudo sed -i "s/define('DB_HOST'.*/define('DB_HOST', '192.168.0.6');/" /var/www/h
 sudo sed -i "s/define('DB_NAME'.*/define('DB_NAME', 'lamp_db');/" /var/www/html/config.php
 sudo sed -i "s/define('DB_USER'.*/define('DB_USER', 'uapp');/" /var/www/html/config.php
 sudo sed -i "s/define('DB_PASSWORD'.*/define('DB_PASSWORD', 'papp');/" /var/www/html/config.php
-
 
 echo "Reiniciando apache"
 systemctl restart apache2
